1ca01a302262993a50c1ede956dac133223fda18
Ahnjunghyeon <1271wndgusdl@naver.com>
2024-05-18 12:40:27 +0900

    4완 5시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,173 @@
-import { CircularProgress } from "@mui/material";
-import { useEffect } from "react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import { WithFirebaseApiProps, withFirebaseApi } from "./Firebase";
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
 import { RootState } from "./redux/store";
-import { setUserId } from "./redux/userSlice";
+import {
+  asyncSetUserInfo,
+  asyncUpdateUserInfo,
+  handleUserChange,
+} from "./redux/userSlice";
 
 const isLoadingState = (state: RootState): boolean => {
   return state.user.userId === undefined;
 };
+const OnboardingBase = (props: WithFirebaseApiProps) => {
+  const userId = useAppSelector((state: RootState) => state.user.userId);
+  const dispatch = useAppDispatch();
+  const [username, setUsername] = useState<string>("");
+  return (
+    <>
+      <Typography variant="h2" component="div" align="left">
+        Finish setting up your account
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <Typography
+          variant="body1"
+          align="left"
+          sx={{ marginTop: "auto", marginBottom: "auto" }}
+        >
+          Username:
+        </Typography>
+        <TextField
+          value={username}
+          label="Edit Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Stack>
+      <Button
+        variant="contained"
+        sx={{ marginTop: 2 }}
+        onClick={async () => {
+          dispatch(
+            asyncSetUserInfo({
+              firebaseApi: props.firebaseApi,
+              userId: userId!,
+              userInfo: { username: username },
+            })
+          );
+        }}
+      >
+        SUBMIT
+      </Button>
+    </>
+  );
+};
 
+const Onboarding = withFirebaseApi(OnboardingBase);
+
+const EditProfileBase = (props: WithFirebaseApiProps) => {
+  const userId = useAppSelector((state: RootState) => state.user.userId);
+  const dispatch = useAppDispatch();
+  const [username, setUsername] = useState<string>("");
+  return (
+    <>
+      <Typography variant="h2" component="div" align="left">
+        Edit Profile
+      </Typography>
+      <Stack direction="row" spacing={2}>
+        <Typography
+          variant="body1"
+          align="left"
+          sx={{ marginTop: "auto", marginBottom: "auto" }}
+        >
+          Username:
+        </Typography>
+        <TextField
+          value={username}
+          label="Edit Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Stack>
+      <Button
+        variant="contained"
+        sx={{ marginTop: 2 }}
+        onClick={async () => {
+          dispatch(
+            asyncUpdateUserInfo({
+              firebaseApi: props.firebaseApi,
+              userId: userId!,
+              userInfo: { username: username },
+            })
+          );
+        }}
+      >
+        SUBMIT
+      </Button>
+    </>
+  );
+};
+
+const EditProfile = withFirebaseApi(EditProfileBase);
+
+const Body = () => {
+  const userId = useAppSelector((state: RootState) => state.user.userId);
+  const userInfo = useAppSelector(
+    (state: RootState) => state.user.userInfo.value
+  );
+  const userInfoLoadState = useAppSelector(
+    (state: RootState) => state.user.userInfo.loadState
+  );
+  if (userId === null) {
+    // logged out user
+    return (
+      <>
+        <Typography>Please Log In</Typography>
+      </>
+    );
+  }
+  if (userInfoLoadState === "loading") {
+    return <CircularProgress />;
+  }
+  if (userInfoLoadState === "failed" || userInfo === undefined) {
+    return (
+      <>
+        <Typography>Something failed</Typography>
+      </>
+    );
+  }
+  if (userInfo === null) {
+    return <Onboarding />;
+  }
+  return (
+    <>
+      <Typography>{`Welcome ${userInfo.username}`}</Typography>
+      <EditProfile />
+    </>
+  );
+};
 const App = (props: WithFirebaseApiProps) => {
   const isLoading = useAppSelector(isLoadingState);
   const dispatch = useAppDispatch();
-
   useEffect(() => {
     return props.firebaseApi.onAuthStateChanged((user) => {
       if (user) {
-        dispatch(setUserId(user.uid));
+        dispatch(handleUserChange(props.firebaseApi, user.uid));
       } else {
-        dispatch(setUserId(null));
+        dispatch(handleUserChange(props.firebaseApi, null));
       }
     });
   }, []);
-
   if (isLoading) {
     return <CircularProgress sx={{ margin: "auto" }} />;
   }
   return (
     <>
       <Header />
+      <Container sx={{ paddingTop: 3 }}>
+        <Box sx={{ margin: "auto" }}>
+          <Body />
+        </Box>
+      </Container>
     </>
   );
 };
-
-export default withFirebaseApi(App);

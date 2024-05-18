0a034bdc91f7a65cdac89b58b87eb31723b5ced3
Ahnjunghyeon <1271wndgusdl@naver.com>
2024-05-18 12:31:12 +0900

    3단계 완 4단계 시작

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC3XQDWcSFSq_dT0SFalTqfbX6gFb0Q33k",
+  authDomain: "react-login-65799.firebaseapp.com",
+  projectId: "react-login-65799",
+  storageBucket: "react-login-65799.appspot.com",
+  messagingSenderId: "279172379194",
+  appId: "1:279172379194:web:8f078dbdd23c14dcb33b03",
+  measurementId: "G-668QK06YCJ",
+};
---
+++ src/Firebase/index.tsx
@@ -0,0 +1,7 @@
+import FirebaseContext, {
+  withFirebaseApi,
+  WithFirebaseApiProps,
+} from "./firebaseContext";
+import FirebaseApi from "./firebaseApi";
+export { FirebaseApi, FirebaseContext, withFirebaseApi };
+export type { WithFirebaseApiProps };
